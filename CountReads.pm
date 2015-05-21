b9cb54ae17aa067480e3f9a39845e2e3566bec7c
Dave Larson <dlarson@genome.wustl.edu>
2015-05-20 22:22:02 -0500

    initial commit

---
+++ CountReads.pm
@@ -0,0 +1,86 @@
+#!/usr/bin/env perl
+package CountReads;
+
+__PACKAGE__->run() unless caller();
+
+use strict;
+use warnings;
+use List::MoreUtils qw(pairwise);
+
+sub run() {
+    my $class = shift;
+    my $variants;
+    my %variant_counts;
+    my $discarded = 0;
+    my $spanning_counts = 0;
+
+    while(<>) {
+        $_ =~ s/\R$//g;
+        unless($variants) {
+            $variants = $class->parse_header($_);
+            next;
+        }
+        my ($ref_name, $read_name, @bases) = split ",", $_, -1;
+        if($class->valid(@bases)) {
+            my @variant_in_read = $class->variants_in_read($variants, @bases);
+            if(@variant_in_read) {
+                my $name = join(",", map {$_->{id}} @variant_in_read);
+                $variant_counts{$name} += 1;
+            }
+            $spanning_counts += 1;
+        }
+        else {
+            $discarded += 1;
+            next;
+        }
+    }
+    print STDOUT "Discarded $discarded non-spanning reads\n";
+    print STDOUT "Evaluated $spanning_counts spanning reads\n";
+    for my $variant (sort keys %variant_counts) {
+        printf "%s\t%d\t%d\t%f\n", $variant, $variant_counts{$variant}, $spanning_counts, $variant_counts{$variant} / $spanning_counts;
+    }
+}
+
+sub parse_header {
+    my ($class, $header_line) = @_;
+    chomp $header_line;
+
+    my @parsed_variants;
+
+    my ($ref_name, $read_name_header, @variants) = split ",", $header_line;
+    for my $variant (@variants) {
+        my ($ref, $pos, $var) = $variant =~ /(\D+)(\d+)(\D+)/;
+        unless(defined $ref and defined $var) {
+            die "Error parsing variant $variant from header\n";
+        }
+        push @parsed_variants, {id => $variant, variant => $var};
+    }
+    return \@parsed_variants;
+}
+
+sub valid {
+    my ($class, @bases) = @_;
+    for my $base (@bases) {
+        if( !defined $base
+            || $base eq ''
+            || $base eq 'N' ) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+sub variant_for_base {
+    my ($class, $position_info, $base) = @_;
+    if($position_info->{variant} eq $base) {
+        return $position_info;
+    }
+    return;
+}
+
+sub variants_in_read {
+    my ($class, $variant_array, @bases) = @_;
+    my @variant_vector = grep { $_ } pairwise { $class->variant_for_base($a, $b) } @$variant_array, @bases;
+    return @variant_vector;
+}
+        
